ceb2df8d481287449ce5855acc3918d0c9d83629
raginda firdaus <ragindafirdaus@gmail.com>
2021-10-04 12:44:20 +0700

    add python version for code comparisson

--- kafka-source-stream/kafka_source_stream.py
+++ kafka-source-stream/kafka_source_stream.py
@@ -48,17 +48,31 @@ if __name__ == "__main__":
         .option("startingOffsets", "earliest") \
         .load()
 
+    # debugging
     # kafka_df.printSchema()
     # kafka_df.select(col('value').cast('string')).show(5, False) # by default, 'value' has binary data type, need to convert to string before show it
     
     value_df = kafka_df.select(from_json(col("value").cast("string"), schema).alias("value"))
     # value_df.printSchema()
     
+    
+    # pyspark sql expression version
     explode_df = value_df.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.PosID", "value.CustomerType",
                                     "value.PaymentMethod", "value.DeliveryType", "value.DeliveryAddress.City",
                                     "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode", 
                                     "explode(value.InvoiceLineItems) as LineItem")
-    
+
+    # python version
+    # explode_df = value_df.select(col("value.InvoiceNumber").alias("InvoiceNumber"), col("value.CreatedTime").alias("CreatedTime"), 
+    #                                 col("value.PosID").alias("PosID"), col("value.CustomerType").alias("CustomerType"),
+    #                                 col("value.PaymentMethod").alias("PaymentMethod"), col("value.DeliveryType").alias("DeliveryType"), 
+    #                                 col("value.DeliveryAddress.City").alias("City"), col("value.DeliveryAddress.State").alias("State"), 
+    #                                 col("value.DeliveryAddress.PinCode").alias("PinCode"), 
+    #                                 explode("value.InvoiceLineItems").alias("LineItem"))
+    # explode_df.show(5, False) # debugging
+
+
+    # pyspark sql expression version
     flattened_df = explode_df \
         .withColumn("ItemCode", expr("LineItem.ItemCode")) \
         .withColumn("ItemDescription", expr("LineItem.ItemDescription")) \
@@ -66,6 +80,17 @@ if __name__ == "__main__":
         .withColumn("ItemQty", expr("LineItem.ItemQty")) \
         .withColumn("TotalValue", expr("LineItem.TotalValue")) \
         .drop("LineItem")
+
+    # python version
+    # flattened_df = explode_df \
+    #     .withColumn("ItemCode", col("LineItem.ItemCode")) \
+    #     .withColumn("ItemDescription", col("LineItem.ItemDescription")) \
+    #     .withColumn("ItemPrice", col("LineItem.ItemPrice")) \
+    #     .withColumn("ItemQty", col("LineItem.ItemQty")) \
+    #     .withColumn("TotalValue", col("LineItem.TotalValue")) \
+    #     .drop("LineItem")
+    # flattened_df.show(5, False) # debugging
+    
     
     invoice_writer_query = flattened_df.writeStream \
         .format("json") \
